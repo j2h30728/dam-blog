0a120a6b0d1acd02a861813dce641d57e96b7cfa
LeeJiHyeon <rachel2148072@gmail.com>
2023-07-10 17:12:11 +0900

    feat: 레이아웃, 메인페이지 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[담의 블로그 만들기]</p>
+        <p>과연 완성할 수 있을지!</p>
+      </section>
+    </Layout>
+  );
 }
